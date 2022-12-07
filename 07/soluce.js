4de9aee56fd6a01112991477c6cd166a34e6cb00
Nicolas <masked@masked.com>
2022-12-07 18:08:49 +0100

    start p7

---
+++ 07/soluce.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+
+/* -- Exec. -- */
+
+console.log("\n" + "--- Advent of Code 2022 ---");
+console.log("--- Day 7: No Space Left On Device ---" + "\n");
+
+console.log("--- Part 1 ---");
+console.log(
+  "Find all of the directories with a total size of at most 100000. What is the sum of the total sizes of those directories?" +
+    "\n"
+);
